29b92b0261971404ab8c19252c4dff4ab611dd34
krishnaanshgoel <151033376+krishnaanshgoel@users.noreply.github.com>
2025-04-13 01:03:47 +0530

    in

--- app/course/[courseId]/flashcards/flashcarditem.jsx
+++ app/course/[courseId]/flashcards/flashcarditem.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
-// import ReactCardFlip from 'react-card-flip'
+import React from 'react'
+import ReactCardFlip from 'react-card-flip'
 
-// function Flashcarditem({isFlipped, handleClick,flashcard}) {
-//   return (
-//     <div className="flex items-center justify-center">
-//       <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-//         <div className="p-4 bg-primary text-white flex items-center justify-center h-[250px] w-[200px] cursor-pointer rounded-lg 
-//         md:h-[350px] md:w-[300px] shadow-lg" onClick={handleClick}>
-//         <h2>  {flashcard.front}</h2>
-//         </div>
+function Flashcarditem({isFlipped, handleClick,flashcard}) {
+  return (
+    <div className="flex items-center justify-center">
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className="p-4 bg-primary text-white flex items-center justify-center h-[250px] w-[200px] cursor-pointer rounded-lg 
+        md:h-[350px] md:w-[300px] shadow-lg" onClick={handleClick}>
+        <h2>  {flashcard.front}</h2>
+        </div>
 
-//         <div className="p-4 bg-white text-black flex items-center justify-center h-[250px] w-[200px] cursor-pointer rounded-lg 
-//         md:h-[350px] md:w-[300px] shadow-lg" onClick={handleClick}>
-//          <h2> {flashcard.back}</h2>
-//         </div>
-//       </ReactCardFlip>
-//     </div>
-//   )
-// }
+        <div className="p-4 bg-white text-black flex items-center justify-center h-[250px] w-[200px] cursor-pointer rounded-lg 
+        md:h-[350px] md:w-[300px] shadow-lg" onClick={handleClick}>
+         <h2> {flashcard.back}</h2>
+        </div>
+      </ReactCardFlip>
+    </div>
+  )
+}
 
-// export default Flashcarditem
+export default Flashcarditem
