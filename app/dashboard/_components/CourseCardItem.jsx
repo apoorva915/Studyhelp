7bf40d25cda138e58a519f05a13744e1a5c6561f
krishnaanshgoel <151033376+krishnaanshgoel@users.noreply.github.com>
2025-04-13 14:59:55 +0530

    in

--- app/dashboard/_components/CourseCardItem.jsx
+++ app/dashboard/_components/CourseCardItem.jsx
@@ -7,13 +7,13 @@ import Link  from 'next/link'
 
 function CourseCardItem({course}) {
   return (
-    <div className="border rounded-lg p-5 shadow-md hover:shadow-lg transition duration-300 ease-in-out">
+    <div className="border rounded-lg p-5 shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full max-w-sm h-full ">
       <div>
         <div className="flex justify-between items-center">
             <Image src={'/knowledge.png'} alt="logo" width={50} height={50} />
             <h2 className="text-[15px] p-1 px-2 rounded-full bg-blue-600 text-white">Date</h2>
         </div>
-        <h2 className="mt-3 text-lg font-medium">{course?.courseLayout?.courseTitle}</h2>
+        <h2 className="mt-3 text-lg font-medium h-[3rem]">{course?.courseLayout?.courseTitle}</h2>
         <p className="text-sm line-clamp-2 text-gray-500 mt-2">{course?.courseLayout?.courseSummary}</p>
         <div className="mt-3">
             <Progress value={10} />
@@ -32,3 +32,4 @@ function CourseCardItem({course}) {
 }
 
 export default CourseCardItem
+
